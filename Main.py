641f3b5be4bcbdcc13150bce044a067d74fe6af4
jaks97 <jkhalilsilva@gmail.com>
2020-06-24 00:36:36 -0300

    Cleaning

--- Main.py
+++ Main.py
@@ -1,26 +1,29 @@
 import os
+from typing import List, Optional
+
 import discord
+from discord import User, Guild, Message, Role, Reaction
 
 TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
 
 
-def get_groups(roles):
+def get_groups(roles: List[Role]) -> List[Role]:
     return list(filter(lambda role: role.name.startswith("Group "), roles))[::-1]
 
 
-def user_group(user):
+def user_group(user: User) -> Optional[Role]:
     return next(iter(get_groups(user.roles)), None)
 
 
-def next_group(guild):
+def next_group(guild: Guild):
     groups = get_groups(guild.roles)
     return min(groups, key=lambda group: len(group.members))
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: Message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
@@ -30,7 +33,7 @@ async def on_message(message):
 
 
 @client.event
-async def on_reaction_add(reaction, user):
+async def on_reaction_add(reaction: Reaction, user: User):
     if user_group(user) is not None:
         await reaction.message.channel.send(user.mention + " You already are part of " + user_group(user).name)
         return
